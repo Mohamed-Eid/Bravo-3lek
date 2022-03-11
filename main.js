b39170b62ea15d8ac80d07cac8e2f2ac88c13e82
Mohamed Eid <medoeid50@gamil.com>
2022-03-11 14:05:35 +0200

    wip

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 // alert("Click anywhere on the page to test playing audio from an extension.")
 $(document).ready(function () { 
 	$(document).on('click',function (e) { 
-		if(e.target.className == "checklist-item-checkbox enabled js-toggle-checklist-item"){
+		if(e.target.className == "checklist-item-checkbox enabled js-toggle-checklist-item" || e.target.className == "checklist-item-checkbox enabled js-toggle-checklist-item pop-checkbox"){
 			let url = chrome.runtime.getURL('bravo.mp3')
-
 			let a = new Audio(url)
-	
 			a.play()
 		}
 	});
